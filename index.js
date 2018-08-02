24889193708726f441c5c10c0719a641cd5c1a1f
davidcantonnoteges <david.canton@noteges.com>
2018-08-02 08:07:54 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,9 +1,19 @@
-import { NativeModules } from "react-native";
+import { NativeModules, Platform } from "react-native";
 
-var RNImmediatePhoneCall = {
-  immediatePhoneCall: function(number, contactId) {
-    NativeModules.RNImmediatePhoneCall.immediatePhoneCall(number, contactId);
-  }
-};
+var RNImmediatePhoneCall;
+
+if (Platform.OS === "android") {
+  RNImmediatePhoneCall = {
+    immediatePhoneCall: function(number, contactId) {
+      NativeModules.RNImmediatePhoneCall.immediatePhoneCall(number, contactId);
+    }
+  };
+} else {
+  RNImmediatePhoneCall = {
+    immediatePhoneCall: function(number) {
+      NativeModules.RNImmediatePhoneCall.immediatePhoneCall(number);
+    }
+  };
+}
 
 export default RNImmediatePhoneCall;
