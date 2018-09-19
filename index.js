7abc958a884d53022b215d269854cfc6104449dc
davidcantonnoteges <david.canton@noteges.com>
2018-09-19 12:47:16 +0200

    Added support for custmers

--- index.js
+++ index.js
@@ -4,11 +4,41 @@ var RNImmediatePhoneCall;
 
 if (Platform.OS === "android") {
   RNImmediatePhoneCall = {
-    immediatePhoneCall: function(number, contactId, userId) {
+    immediatePhoneCall: function(
+      number,
+      contactId,
+      userId,
+      personId,
+      propertyId,
+      demandId
+    ) {
+      if (typeof contactId === "undefined") {
+        contactId = -1;
+      }
+
+      if (typeof userId === "undefined") {
+        userId = -1;
+      }
+
+      if (typeof personId === "undefined") {
+        personId = -1;
+      }
+
+      if (typeof propertyId === "undefined") {
+        propertyId = -1;
+      }
+
+      if (typeof demandId === "undefined") {
+        demandId = -1;
+      }
+
       NativeModules.RNImmediatePhoneCall.immediatePhoneCall(
         number,
         contactId,
-        userId
+        userId,
+        personId,
+        propertyId,
+        demandId
       );
     }
   };
